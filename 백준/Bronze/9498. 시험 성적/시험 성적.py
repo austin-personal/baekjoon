873358b730a2f4d4ae4f732826874893ae4828e3
Austin <72653263+austin-personal@users.noreply.github.com>
2024-07-05 15:45:59 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+grade = int(input())
+if grade >=90:
+  print("A")
+elif grade <= 69 and grade >=60:
+  print("D")
+elif grade <= 79 and grade >=70:
+  print("C")
+elif grade <= 89 and grade >=80:
+  print("B")
+else:
+  print("F")

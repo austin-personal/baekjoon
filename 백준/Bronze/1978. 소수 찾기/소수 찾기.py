471e148d4bc02c723fa6b990f82135da6ecb56e8
changmo <kmaestro043@gmail.com>
2024-07-05 16:25:00 +0900

    Update question

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,12 +1,12 @@
 N=int(input())
 nums = map(int, input().split())
-ss=0
+p_num=0
 for num in nums:
   if num>1:
-    error=0
+    count=0
     for i in range(2,num):
       if num%i == 0:
-        error+=1
-    if error == 0:
-      ss+=1
-print(ss)
+        count+=1
+    if count == 0:
+      p_num+=1
+print(p_num)

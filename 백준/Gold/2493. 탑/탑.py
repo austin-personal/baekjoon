26f2efbff15996f93f83d017e4e6475f1f2aa308
changmo <kmaestro043@gmail.com>
2024-07-11 09:57:16 +0900

    asdf

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -6,8 +6,10 @@ def telCom():
     stack = []
 
     for i in range(N):
+        # 스택에 맨 마지막 오브젝트(인덱스)에 해당하는, "현재 스택에 담긴" 탑이 현재 탑 보다 작을시에 현재 있던 스택의 마지막을 뺀다. 
         while stack and top_list[stack[-1]] < top_list[i]:
             stack.pop()
+        # 현재 탑에 대한 인덱스 결과 값에 스택의 마지막 인덱스에 +1을 한 수를 replace한다. 
         if stack:
             result[i] = stack[-1] + 1
         stack.append(i)
@@ -16,3 +18,40 @@ def telCom():
 
 ans = telCom()
 print(' '.join(map(str, ans)))
+
+
+## 결국 스택이란 가장 큰 탑과 그 탑에 비치는 탑들이 담긴다. 
+
+##############################################################################
+
+## 첫번째 시도: 시간 복잡도가 O(n^2)이라 타임 아웃
+N = int(input())
+top_list = list(map(int, input().split()))
+
+
+def telCom():
+    result = []
+    for i in range(N, 0, -1):
+        if top_list[i-1] <= top_list[i - 2]:
+            result.append(i-1)
+        elif top_list[i-1] > top_list[i - 2]:
+            change = 0
+            for j in range(i-2, 0, -1):
+                if top_list[i-1] <= top_list[j]:
+                    result.append(j+1)
+                    change+=1
+                    continue
+            if change==0:
+                result.append(0)
+
+    return result
+
+t_list = telCom()
+ans = []
+for i in range(len(t_list),0,-1):
+  ans.append(t_list[i-1])
+print(ans)
+  
+  
+
+
--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -11,7 +11,7 @@ while queue:
     idx += k - 1  # k-1번째 인덱스까지 건너뛰기
     if idx >= len(queue):  # 인덱스가 범위를 넘어갈 경우
         idx %= len(queue)  # 나머지 연산을 통해 인덱스 계산
-
+        print(idx)
     res.append(str(queue.pop(idx)))  # k번째 수 제거 후 결과 배열에 추가
 
 # 결과 출력

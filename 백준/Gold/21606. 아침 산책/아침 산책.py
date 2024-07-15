4fa1290b316d83dd20873fffc7687577d10ee205
Austin <72653263+austin-personal@users.noreply.github.com>
2024-07-15 22:05:19 +0900

    [Gold III] Title: 아침 산책, Time: 800 ms, Memory: 102136 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,45 +1,42 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6)
 
-def dfs(node):
-    global cnt
-    visited[node] = True
-    route.append(node)
+N = int(input())
+inside = '0' + input()  # 각 노드가 실내(1)인지 실외(0)인지 저장한 문자열
+# 노드 번호를 index로 접근하기 위해 앞에 0 추가
 
-    if inOut[node - 1] == 1 and len(route) > 1:  # Found a valid route
-        cnt += 1
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+total = 0 # 경로의 개수를 저장할 변수
 
-    for neighbor in adj_dict[node]:
-        if not visited[neighbor]:
-            if (inOut[node - 1] == 1 and len(route) == 1) or inOut[node - 1] == 0:
-                dfs(neighbor)
 
-    route.pop()
-    visited[node] = False
+for _ in range(N-1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+    # 두 정점 모두 실내이면 경로의 수 2 증가
+    if inside[a] == "1" and inside[b] == "1":
+        total += 2
+    
 
-# Get number of nodes
-n = int(sys.stdin.readline().strip())
+def DFS(start):
+    visited[start] = True # 방문 처리
+    inside_count = 0 # 현재 노드와 인접한 실내 노드 개수를 저장할 변수
+    for v in graph[start]:
+        # 인접한 노드가 실내라면
+        if inside[v] == '1': 
+            inside_count += 1 # 실내 노드 개수 증가
 
-# Get in or out data
-inOut = list(map(int, sys.stdin.readline().strip()))
+        # 인접한 노드가 실외라면
+        elif not visited[v] and inside[i] == "0": # 그 노드에서부터 탐색하여 실내 노드 개수 증가
+            inside_count += DFS(v)
+    return inside_count # 실내 노드 개수 반환
 
-# Initialize adj_dict
-adj_dict = {i: [] for i in range(1, n + 1)}
 
-# Build adj_dict
-for _ in range(n - 1):
-    a, b = map(int, sys.stdin.readline().strip().split())
-    adj_dict[a].append(b)
-    adj_dict[b].append(a)
+for i in range(1, N+1):
+    if inside[i] == '0' and not visited[i]: # 시작이 실외일 때만 탐색
+        result = DFS(i) # DFS를 통해 인접한 실내 노드 개수를 계산
+        total += (result) * (result - 1) # 경로의 개수 추가
 
-# Initialize visited array and other variables
-visited = [False] * (n + 1)
-route = []
-cnt = 0
-
-# Perform DFS for each node that could be a starting point
-for i in range(1, n + 1):
-    if inOut[i - 1] == 1:
-        dfs(i)
-
-print(cnt)
+print(total)
